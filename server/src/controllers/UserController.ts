8d6802d20c75eecb0e6d0e1da39736272ebe803b
Gabriel Prando <gprando55@gmail.com>
2020-08-02 11:23:40 -0300

    feat: add description em  features

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -9,14 +9,18 @@ export default class UserController {
     async create(request: Request, response: Response) {
         const { name, email, password } = request.body;
 
+        // procura se existe um usuário com o mesmo email já
         const userExists = await User.findOne({ email });
 
+        // se existir retorna erro de 401 de não autorizado
         if (userExists) {
           return response.status(401).json({ error: 'User already exists' });
         }
 
+        // criptografa a senha
         const hashedPassword = await bcrypt.hash(password, 8);
         
+        // cria e salva o usuário com a senha criptografada
         const user = await User.create({
             name,
             email,
