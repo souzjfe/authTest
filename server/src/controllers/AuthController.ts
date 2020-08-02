65ab57b6f506db7fe835e0766147b023653e79b5
jefer <jefer@pop-os.localdomain>
2020-08-02 15:00:04 -0300

    up auth

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -11,16 +11,16 @@ interface userTypes {
 
 export default class AuthController {
 
-
-
+    
+    
     async autenticate(request:Request, response: Response){
         const {email, password } = request.body;
-
         const user = await User.findOne({ email });
-        if (!user) return response.status(401).send({error: " user not found"});
-
-        return response.send(user);
-        if (!await bcrypt.compare(password, user.password))
+        if (!user) 
+            return response.status(401).send({error: " user not found"});
+    
+        if (!await bcrypt.compare(password,user.password)) 
+            return response.status(401).send({error:"invalid Password"});
 
 
 
